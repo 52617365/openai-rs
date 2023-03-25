b756d0be17f8446e5fc174fe7cff90e1f7efa332
52617365 <rasengithub@protonmail.com>
2023-03-25 14:21:12 +0200

    We now have a function to serialize questions.

--- src/api/chat.rs
+++ src/api/chat.rs
@@ -1,20 +1,25 @@
+use serde::{Deserialize, Serialize};
 use std::io::{self, BufRead};
-
-const API_URL: &str = "https://api.openai.com/v1/chat/completions";
+const API_URL: &str = "https://api.openai.com/v1/chat/completions/";
 const MODEL: &str = "gpt-3.5-turbo";
 const REGULAR_LINE_BREAK: &str = "\r\n";
 
+#[derive(Serialize, Deserialize)]
 pub enum Role {
+    #[serde(rename = "system")]
     System(String),
+    #[serde(rename = "user")]
     User(String),
+    #[serde(rename = "assistant")]
     Assistant(String),
 }
+
 pub fn ask_user_for_questions() -> Vec<Role> {
     let stdin = io::stdin();
     let mut questions = vec![];
 
     println!("Prefixes: s_{{question}}, a_{{question}}, default is set to user question.");
-    println!("s_ is system, a_ is assistant.");
+    println!("s_ stands for system, a_  for assistant respectively.");
     println!("--------------------------------");
     println!("Enter questions. Press enter with no text to go forward.");
 
@@ -26,7 +31,6 @@ pub fn ask_user_for_questions() -> Vec<Role> {
             break;
         } else {
             let categorized_question = categorize_question(&temp_question);
-
             questions.push(categorized_question);
         }
     }
@@ -37,20 +41,27 @@ pub fn ask_user_for_questions() -> Vec<Role> {
 fn categorize_question(question: &str) -> Role {
     let create_copy = |ref_string: &str| return ref_string.trim().to_string();
 
-    let remove_prefix = |og_string: &str| {
-        let (_, trimmed_string) = og_string.split_at(2);
+    let remove_prefix = |ref_string: &str| {
+        let (_, trimmed_string) = ref_string.split_at(2);
         return trimmed_string.to_string();
     };
+
     if question.starts_with("s_") {
         let question_copy = create_copy(question);
         let remove_prefix = remove_prefix(&question_copy);
-        return Role::System(remove_prefix.to_string());
+        return Role::System(remove_prefix);
     } else if (*question).starts_with("a_") {
         let question_copy = create_copy(question);
         let remove_prefix = remove_prefix(&question_copy);
-        return Role::Assistant(remove_prefix.to_string());
+        return Role::Assistant(remove_prefix);
     } else {
         let question_copy = create_copy(question);
         return Role::User(question_copy);
     }
+}
+
+pub fn construct_payload(questions: &Vec<Role>) -> String {
+    let serialized_vector = serde_json::to_string(&questions).unwrap();
+
+    return serialized_vector;
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,7 @@ mod api;
 use crate::api::chat::ask_user_for_questions;
 fn main() {
     let questions = ask_user_for_questions();
-    for question in questions {
-        match question {
-            api::chat::Role::Assistant(some_string) => println!("Assistant - {}", some_string),
-            api::chat::Role::User(some_string) => println!("User - {}", some_string),
-            api::chat::Role::System(some_string) => println!("System - {}", some_string),
-        }
-    }
+    let payload = api::chat::construct_payload(&questions);
+
+    println!("{}", payload);
 }

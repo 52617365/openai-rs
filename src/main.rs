d24574f98238c30a7b3af6713aa83c664e79a1f3
52617365 <rasengithub@protonmail.com>
2023-03-26 15:41:23 +0300

    usage committed

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ fn main() {
     let questions = ask_user_for_questions();
     let _ = match api::chat::send_request_to_api(&questions) {
         Ok(res) => println!("{:?}", res),
-        Err(_) => println!("{}", "There was an error sending request to OpenAI."),
+        Err(e) => println!("{}", e),
     };
 }
